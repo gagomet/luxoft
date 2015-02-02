012a42059e121c1f210165e6933761976976b7cc
gagomet <kir_one@list.ru>
2015-02-02 23:14:40 +0200

    in-memory DB uses iBatis ScriptRunner

--- src/main/java/BankApplication/service/impl/TestService.java
+++ src/main/java/BankApplication/service/impl/TestService.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 
 /**
